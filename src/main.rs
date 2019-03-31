be6324051766ffe30d679842f68c8e111a8f2fa1
Marcus Ramberg <marcus.ramberg@gmail.com>
2019-03-31 14:23:55 +0200

    Ball bounces back and forth. score keeping in place

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,8 @@ pub struct App {
     window: PistonWindow, // OpenGL drawing backend.
     ball_x: f64,
     ball_y: f64,
+    current_width: f64,
+    current_height: f64,
     left_paddle: f64,
     right_paddle: f64,
     ball_direction: f64,
@@ -21,6 +23,8 @@ impl App {
             window: window,
             ball_x: 0.0,
             ball_y: 0.0,
+            current_width: 0.0,
+            current_height: 0.0,
             left_paddle: 0.0,
             right_paddle: 0.0,
             ball_direction: 30.0,
@@ -33,7 +37,7 @@ impl App {
         const BLACK: [f32; 4] = [0.0, 0.0, 0.0, 1.0];
         const WHITE:   [f32; 4] = [1.0, 1.0, 1.0, 1.0];
         const GRAY:   [f32; 4] = [0.6, 0.6, 0.6, 1.0];
-        const FONTSIZE : f64 = 20.0;
+        const FONTSIZE : u32 = 48;
 
         const BALL_SIZE: f64 = 20.0;
 
@@ -41,8 +45,10 @@ impl App {
                       args.height / 2.0);
         let ball= rectangle::square(self.ball_x, self.ball_y, BALL_SIZE);
 
-        let mut _left_score=self.score[0].to_string();
-        let mut _right_score=self.score[1].to_string();
+        let mut _left_score = self.score[0].to_string();
+        let mut _right_score = self.score[1].to_string();
+        self.current_height = args.height;
+        self.current_width = args.width;
 
 
         self.window.draw_2d(event, |c, g| {
@@ -50,7 +56,6 @@ impl App {
             clear(BLACK, g);
 
 
-            // Draw a box rotating around the middle of the screen.
             for i in 1 .. 20 {
                 let strip_y=(args.height / BALL_SIZE) * i as f64;
                 let strip = rectangle::square(center_x, strip_y, BALL_SIZE);
@@ -58,12 +63,19 @@ impl App {
             }
             rectangle(WHITE, ball, c.transform, g);
             // Draw the score
-            let transform = c.transform.trans(10.0, 100.0);
-              text::Text::new_color(WHITE, 32).draw(
+            let left_score_pos = c.transform.trans((args.width/10.0)*4.0, 100.0);
+              text::Text::new_color(WHITE, FONTSIZE).draw(
                 &_left_score,
                 glyphs,
                 &c.draw_state,
-                transform, g
+                left_score_pos, g
+            ).unwrap();
+            let right_score_pos = c.transform.trans((args.width/10.0)*6.0, 100.0);
+              text::Text::new_color(WHITE, FONTSIZE).draw(
+                &_right_score,
+                glyphs,
+                &c.draw_state,
+                right_score_pos, g
             ).unwrap();
             //let text_image = Image::new_color(WHITE);
            // text_image.build
@@ -94,7 +106,22 @@ impl App {
         // move ball
         self.ball_x = self.ball_x + 10.0 * (self.ball_direction * PI / 180.0).cos();
         self.ball_y = self.ball_y + 10.0 * (self.ball_direction* PI / 180.0).sin();
+        if self.current_width > 0.0 && self.ball_x>self.current_width {
+            self.score[0]=self.score[0]+1;
+            self.reverse_direction();
+        }
+        if self.ball_x < 0.0 {
+            self.score[1]=self.score[1]+1;
+            self.reverse_direction();
+        }
 
+    }
+
+    fn reverse_direction(&mut self) {
+        self.ball_direction=self.ball_direction+180.0;
+        if self.ball_direction>360.0 {
+            self.ball_direction=self.ball_direction % 360.0;
+        }
     }
 }
 
